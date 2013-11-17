7b64f7cd01d7306460b71a63ab78842545c5224f
notaz <notasas@gmail.com>
2013-11-17 02:07:32 +0200

    more complete disassembly

--- run_mkpubinc.sh
+++ run_mkpubinc.sh
@@ -6,5 +6,13 @@ cat StarCraft.asm | fromdos | sed -e \
 '1,/^_rdata.*segment/d;/^_data_last/q;/^[[:blank:];]/d;/^;/d;/^_r\?data\>/d;' | awk '{print $1}' | \
 while read a; do
   test -z "$a" && continue
+  case $a in
+  __IMPORT_DESCRIPTOR*)
+    continue
+    ;;
+  *)
+    ;;
+  esac
+
   echo "PUBLIC $a" >> public.inc
 done
